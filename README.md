ca0768a29169e690fc6730916939fa7982db0f69
Giovanni Malcolm <malcolm.giovanni@gmail.com>
2021-11-07 17:21:53 -0500

    README and stuff

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# eCommerce-backend
+# Ecommerce Backend
+  ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+       https://opensource.org/licenses/MIT
+
+  ## Description
+
+Backend build for an e-commerce site. Modified started code using Express.js API and Sequelize. A [walk-through video](https://drive.google.com/file/d/10MUEg79rjHjjKZLfFrJRtiASSW2a87ha/view?usp=sharing) is provided for usage.
+
+
+## Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command: 
+
+```
+npm i
+```
+
+## Usage
+
+Please add a ".env" file with your local credentials to access the database with mysql. Required variables are DB_USER, DB_PW, and DB_NAME. 
+
+## License 
+
+This project is licensed under the MIT license.
+
+## Contributing 
+
+N/A
+
+## Tests
+
+To run tests, run the following command:
+
+```
+N/A
+```
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at malcolm.giovanni@gmail.com. 
+You can find more of my work at ![giovannimalcolm](https://github.com/giovannimalcolm/).
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,34 +5,37 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   Tag.findAll({
-    include: [Product, ProductTag],
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
+      },
+    ],
   })
-  .then((tags) => {
-    res.json(tags)})
-    .catch((err) => {
-    res.status(500).json(err)
-  });
+    .then((tags) => res.status(200).json(tags))
+    .catch((err) => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
   Tag.findOne({
-    include: [Product, ProductTag],
     where: {
       id: req.params.id,
-    }
+    },
+    include: [
+      {
+        model: Product,
+        through: ProductTag,
+      },
+    ],
   })
-  .then((tag) => {
-    res.json(tags)
-  })
-  .catch((err) => {
-    res.status(500).json(err)
-  })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
 });
 
 router.post('/', (req, res) => {
   Tag.create(req.body)
   .then((tag) => res.status(200).json(tag))
-  .catch((err) => res.status(400).json(err))
+  .catch((err) => res.status(404).json(err))
 });
 
 router.put('/:id', (req, res) => {
@@ -42,7 +45,7 @@ router.put('/:id', (req, res) => {
     }
    })
    .then((tag) => res.status(200).json(tag))
-  .catch((err) => res.status(400).json(err))
+  .catch((err) => res.status(404).json(err))
 });
 
 router.delete('/:id', (req, res) => {
@@ -52,6 +55,6 @@ router.delete('/:id', (req, res) => {
     }
    })
    .then((tag) => res.status(200).json(tag))
-  .catch((err) => res.status(400).json(err))
+  .catch((err) => res.status(404).json(err))
   })
 module.exports = router;
